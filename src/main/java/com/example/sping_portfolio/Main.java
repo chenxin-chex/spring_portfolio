7e4b125e1eabb7b288d069ed6d8b2c1e7d048b9f
rachelklee <77082379+rachelklee@users.noreply.github.com>
2021-09-01 08:22:06 -0700

    minilab1

--- src/main/java/com/example/sping_portfolio/Main.java
+++ src/main/java/com/example/sping_portfolio/Main.java
@@ -4,8 +4,36 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class Main {
 
-    public static void main(String[] args) {SpringApplication.run(Main.class, args);}
+    public static void main(String[] args) {
 
+        double grade1 = 99.00 ;
+
+        double grade2 = 78.00 ;
+
+        double grade3 = 66.00 ;
+
+        double grade4 = 98.00 ;
+
+        double grade5 = 75.00 ;
+
+        double grade6 = (grade1 + grade2)/2 ;
+
+        double grade7 = (grade3 + grade4)/2 ;
+
+        double grade8 = (grade6 + grade7 + grade5/2)/3 ;
+
+        System.out.println("grade6 is " + grade6);
+
+        System.out.println("grade7 is " + grade7);
+
+        System.out.println("grade8 is " + grade8);
+
+
+    }
 }
+
+
+
